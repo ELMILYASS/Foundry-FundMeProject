0f61424de5633e4e0ea62f64d0e92a65d8aba833
‘ELMILYASS’ <ilyasselmazidi123@gmail.com>
2023-10-22 14:25:59 +0100

    modules

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    event HelperConfig__CreatedMockPriceFeed(address priceFeed);
+
+    constructor() {
+        // when we will deploy our contract we specify on each chain we are ( by default anvil)
+        // if we are on sepolia chain we will use Sepolia config
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            //else we will use Anvil ( default chain ) config
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig()
+        public
+        pure
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+        });
+    }
+
+    // in the case of local deployement we need to deploy first an ETH/USD contract to our local blockchain ( it is the MockV3Aggregator)
+    function getOrCreateAnvilEthConfig()
+        public
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        //if already deployed
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+        emit HelperConfig__CreatedMockPriceFeed(address(mockPriceFeed));
+
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+    }
+}
